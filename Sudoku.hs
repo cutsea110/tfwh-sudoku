579f884b0326f08380d26ef0ff1c085a0859c75f
cutsea110 <cutsea110@gmail.com>
2017-12-04 06:50:39 +0900

    succeed to solve

--- Sudoku.hs
+++ Sudoku.hs
@@ -13,7 +13,7 @@ blank :: Digit -> Bool
 blank = (=='0')
 
 solve :: Grid -> [Grid]
-solve = filter valid . completions
+solve = filter valid . expand . many prune . choices
 
 completions :: Grid -> [Grid]
 completions = expand . choices
@@ -26,8 +26,11 @@ type Choices = [Digit]
 choices :: Grid -> Matrix Choices
 choices = map (map choice)
 
+choice :: Digit -> [Digit]
 choice d = if blank d then digits else [d]
 
+cp :: [[a]] -> [[a]]
+cp []         = [[]]
 cp (xs : xss) = [x : ys | x <- xs, ys <- yss]
   where
     yss = cp xss
@@ -55,3 +58,35 @@ group xs = take 3 xs : group (drop 3 xs)
 
 ungroup :: [[a]] -> [a]
 ungroup = concat
+
+prune :: Matrix Choices -> Matrix Choices
+prune = pruneBy boxs . pruneBy cols . pruneBy rows
+
+pruneBy :: ([Row Choices] -> [Row Choices]) -> [Row Choices] -> [Row Choices]
+pruneBy f = f . map pruneRow . f
+
+many :: (Eq a) => (a -> a) -> a -> a
+many f x = if x == y then x else many f y
+  where
+    y = f x
+
+pruneRow :: Row Choices -> Row Choices
+pruneRow row = map (remove fixed) row
+  where
+    fixed = [d | [d] <- row]
+
+remove :: Choices -> Choices -> Choices
+remove ds [x] = [x]
+remove ds xs  = filter (`notElem` ds) xs
+
+example :: Grid
+example = [ "004005700"
+          , "000009400"
+          , "360000008"
+          , "720060000"
+          , "000402000"
+          , "000080093"
+          , "400000056"
+          , "005300000"
+          , "006100900"
+          ]
